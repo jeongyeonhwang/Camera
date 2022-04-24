d020aa639a19fbae63633cdb0544531a1d9e945f
jeongyeonhwang <ouo0222@naver.com>
2022-04-25 00:24:00 +0900

    cameratest

--- app/src/main/java/com/example/practice3/MainActivity.java
+++ app/src/main/java/com/example/practice3/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.app.usage.NetworkStats;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -20,6 +21,14 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.commons.logging.Log;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.normal.TedPermission;
 
@@ -35,11 +44,18 @@ public class MainActivity extends AppCompatActivity {
     private String imageFIlePath;
     private Uri photoUri;
     private Object permissionListener;
+    private FirebaseAuth mAuth;
+    private FirebaseStorage storage;
+    //private final DatabaseReference root = FirebaseDatabase.getInstance().getReference("images");
+    //private final StorageReference reference = FirebaseStorage.getReference();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //FirebaseStorage storage = FirebaseStorage.getInstance();  // [START storage_field_initialization]
+        //mStorageRef = FirebaseStorage.getInstance().getReference();
 
         PermissionListener permissionListener = new PermissionListener() {
             @Override
@@ -65,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn_capture).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CountDownTimer CDT = new CountDownTimer(10*1000, 3*1000) { //10(10*1000)초동안 3초마다 실행
+                CountDownTimer CDT = new CountDownTimer(10*1000, 4*1000) {//10(10*1000)초동안 3초마다 실행
                     @Override
                     public void onTick(long l) {  //반복실행할 구문
                         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -77,35 +93,45 @@ public class MainActivity extends AppCompatActivity {
                             } catch (IOException e) {
 
                             }
-
                             if (photoFile != null) {
+                                //Uri photoUri = Uri.fromFile(new File("path/to/images"));
+                                //StorageReference riversRef = storageRef.child("images");
                                 photoUri = FileProvider.getUriForFile(getApplicationContext(), getPackageName(), photoFile);
                                 intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
                                 startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+                                Toast.makeText(getApplicationContext(), "촬영되었습니다", Toast.LENGTH_SHORT).show();
                             }
                         }
-                         System.out.println("촬영되었습니다.");
+
+                        //DB에 등록하기
+                        //mAuth = FirebaseAuth.getInstance();
+                        final String cu = mAuth.getUid();
+                        //사진을 storage에 저장하고 url 가져옴
+                        String filename = cu + "_" + System.currentTimeMillis();
+                        //StorageReference storageRef = storage.getReferenceFromUrl("https://console.firebase.google.com/project/python-connection-1692b/storage/python-connection-1692b.appspot.com/files").child("images/" + filename);
+                        StorageReference storageRef = storage.getReferenceFromUrl("https://console.firebase.google.com/project/python-connection-1692b/storage/python-connection-1692b.appspot.com/files").child("images/" + filename);
+                        Uri file = null;
+                        file = photoUri;
+                        UploadTask uploadTask = storageRef.putFile(file);
                     }
 
                     @Override
                     public void onFinish() {  // 마지막에 실행할 구문
-                        System.out.println("타이머가 종료되었습니다.");
+                        Toast.makeText(getApplicationContext(), "타이머가 종료되었습니다", Toast.LENGTH_SHORT).show();
                     }
                 };
 
                 CDT.start(); //CDT 실행
                 CDT.cancel(); //CDT 종료
 
-            }
+            } //onClick
         });
 
+    } //oncreate
 
 
-
-
-    }
-
-    private File createImageFile() throws IOException {
+    private File createImageFile() throws IOException { //이미지 생성 함수
+        /***
         // create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "TEST_" + timeStamp + "_";
@@ -116,7 +142,31 @@ public class MainActivity extends AppCompatActivity {
         );
         if(!image.exists()) image.createNewFile();
         imageFIlePath = image.getAbsolutePath();
-        return image;
+        return image; ***/
+        String imageFileName = System.currentTimeMillis() + ".jpg";
+        File imageFile= null;
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/Pictures", "ireh");
+
+        imageFile = new File(storageDir,imageFileName);
+        String mCurrentPhotoPath = imageFile.getAbsolutePath();
+        return imageFile;
+
+         /***
+        String imgFileName = System.currentTimeMillis() + ".jpg";
+        File imageFile= null;
+        File storageDir = new File(Environment.getExternalStorageDirectory() + "/Pictures", "ireh");
+
+        if(!storageDir.exists()){
+            //없으면 만들기
+            Log.v("알림","storageDir 존재 x " + storageDir.toString());
+            storageDir.mkdirs();
+        }
+        Log.v("알림","storageDir 존재함 " + storageDir.toString());
+        imageFile = new File(storageDir,imgFileName);
+        mCurrentPhotoPath = imageFile.getAbsolutePath();
+
+        return imageFile;
+          ***/
     }
 
     @Override
